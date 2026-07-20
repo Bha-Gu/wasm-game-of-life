ae6df7c161580a010fc8bbb5069d03fca543d1b7
bha-gu <bhaumik050402@gmail.com>
2026-07-20 13:07:46 +0530

    initial commit

---
+++ docs/index.js
@@ -0,0 +1,85 @@
+import { Universe } from "wasm-game-of-life";
+
+const CELL_SIZE = 5; // px
+const GRID_COLOR = "#CCCCCC";
+const DEAD_COLOR = "#000000";
+const ALIVE_COLOR = "#FFFFFF";
+
+// Construct the universe, and get its width and height.
+const universe = Universe.new();
+const width = universe.width();
+const height = universe.height();
+
+// Give the canvas room for all of our cells and a 1px border
+// around each of them.
+const canvas = document.getElementById("game-of-life-canvas");
+canvas.height = (CELL_SIZE + 1) * height + 1;
+canvas.width = (CELL_SIZE + 1) * width + 1;
+
+const ctx = canvas.getContext("2d");
+
+const FPS = 10; // ticks per second
+
+const renderLoop = () => {
+  setTimeout(() => {
+    universe.tick();
+    console.log("Tick");
+    drawGrid();
+    drawCells();
+    requestAnimationFrame(renderLoop);
+  }, 1000 / FPS);
+};
+const drawGrid = () => {
+  ctx.beginPath();
+  ctx.strokeStyle = GRID_COLOR;
+
+  // Vertical lines.
+  for (let i = 0; i <= width; i++) {
+    ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+    ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+  }
+
+  // Horizontal lines.
+  for (let j = 0; j <= height; j++) {
+    ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
+    ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
+  }
+
+  ctx.stroke();
+};
+// Import the WebAssembly memory at the top of the file.
+import { memory } from "wasm-game-of-life/wasm_game_of_life_bg.wasm";
+
+// ...
+
+const getIndex = (row, column) => {
+  return row * width + column;
+};
+
+const drawCells = () => {
+  const cellsPtr = universe.cells();
+  const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+
+  ctx.beginPath();
+
+  for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+      const idx = (getIndex(row, col) / 8) | 0;
+      const bit = getIndex(row, col) % 8;
+      ctx.fillStyle =
+        (cells[idx] & (2 ** bit)) === 0 ? DEAD_COLOR : ALIVE_COLOR;
+
+      ctx.fillRect(
+        col * (CELL_SIZE + 1) + 1,
+        row * (CELL_SIZE + 1) + 1,
+        CELL_SIZE,
+        CELL_SIZE,
+      );
+    }
+  }
+
+  ctx.stroke();
+};
+drawGrid();
+drawCells();
+requestAnimationFrame(renderLoop);
